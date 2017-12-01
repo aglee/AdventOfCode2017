f92f6fd4c88077bfd9360dba8fb767ab4e9a6655
Andy Lee <aglee@mac.com>
2017-12-01 03:36:07 -0500

    Minor: moved optional unwrapping.

--- Day 01 - Inverse Captcha/main.swift
+++ Day 01 - Inverse Captcha/main.swift
@@ -1,13 +1,13 @@
 // inputLines contains the input data, an array of strings.
 
-let digits = Array(inputLines[0]).map({ Int(String($0)) })
+let digits = Array(inputLines[0]).map({ Int(String($0))! })
 let numDigits = digits.count
 
 func solve1() {
 	var total = 0
 	for i in 0..<numDigits {
 		if digits[i] == digits[(i+1) % numDigits] {
-			total += digits[i]!
+			total += digits[i]
 		}
 	}
 	print(total)
@@ -17,7 +17,7 @@ func solve2() {
 	var total = 0
 	for i in 0..<numDigits {
 		if digits[i] == digits[(i + (numDigits/2)) % numDigits] {
-			total += digits[i]!
+			total += digits[i]
 		}
 	}
 	print(total)
