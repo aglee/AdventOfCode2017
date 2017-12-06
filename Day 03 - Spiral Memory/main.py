0ce8fa6d4913080ebedbe27e8d70570ed24c99f7
Andy Lee <aglee@mac.com>
2017-12-06 03:26:45 -0500

    Fiddled with the Python code. A little clearer now IMO.

--- Day 03 - Spiral Memory/main.py
+++ Day 03 - Spiral Memory/main.py
@@ -2,35 +2,34 @@
 
 inputNum = 289326
 
+def left(x, y): return (x - 1, y)
+def right(x, y): return (x + 1, y)
+def up(x, y): return (x, y + 1)
+def down(x, y): return (x, y - 1)
+
 def nextXY(x, y):
     """
     Returns the coordinates of the next cell in the spiral.  The point (x, y)
-    is considered to be on the perimeter of a square centered at (0, 0).
+    is considered to be on the perimeter of a square centered at (0, 0).  We
+    get to the next cell by traveling counterclockwise on that perimeter.
     """
     
     # Special case: the origin.
-    if (x, y) == (0, 0):
-        return (1, 0)
+    if (x, y) == (0, 0): return (1, 0)
 
     # Special case: a corner.
     if abs(x) == abs(y):
         if y > 0:
-            # Top right and top left corners.
-            return (x - 1, y) if x > 0 else (x, y - 1)
-        else:
-            # Bottom two corners.
-            return (x + 1, y)
+            if x > 0: return left(x, y)  # Top right.
+            else: return down(x, y)  # Top left.
+        else: return right(x, y)  # Bottom two corners.
 
     # Remaining cases.  Ironically, they are "edge" (of the square)
     # cases but not edge cases. :)
-    if x > 0 and abs(y) < x:
-        return (x, y + 1)  # Right edge (excluding corners).
-    if y > 0 and abs(x) < y:
-        return (x - 1, y)  # Top edge (excluding corners).
-    if x < 0 and abs(y) < abs(x):
-        return (x, y - 1)  # Left edge (excluding corners).
-    if y < 0 and abs(x) < abs(y):
-        return (x + 1, y)  # Bottom edge (excluding corners).
+    if x > 0 and abs(y) < x: return up(x, y)  # Right edge.
+    if y > 0 and abs(x) < y: return left(x, y)  # Top edge.
+    if x < 0 and abs(y) < abs(x): return down(x, y)  # Left edge.
+    if y < 0 and abs(x) < abs(y): return right(x, y)  # Bottom edge.
 
 def solve1():
     (x, y) = (0, 0)
@@ -39,7 +38,7 @@ def solve1():
         (x, y) = nextXY(x, y)
         value += 1
         if value == inputNum:
-            print(abs(x) + abs(y))
+            print(abs(x) + abs(y))  # Manhattan distance.
             return
 
 def solve2():
