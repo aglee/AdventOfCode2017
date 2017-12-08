f5f241c5da82328202be8c1a113bc2c8e1c70e03
Andy Lee <aglee@mac.com>
2017-12-08 01:38:03 -0500

    Added "testInputLines" to input file. Can paste example input there.

--- Day XX/input.py
+++ Day XX/input.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# Example input.
+testInputLines = """PASTE_ACTUAL
+INPUT_HERE""".split("\n")
+
+# Real input.
 inputLines = """PASTE_ACTUAL
 INPUT_HERE""".split("\n")
 
--- Day XX/input.swift
+++ Day XX/input.swift
@@ -1,6 +1,11 @@
 import Foundation
 
-// Puzzle input copied from the AoC website.
+// Example input.
+let testInputLines: [String] = """
+PASTE_ACTUAL_INPUT_HERE
+""".components(separatedBy: .newlines)
+
+// Real input.
 let inputLines: [String] = """
 PASTE_ACTUAL_INPUT_HERE
 """.components(separatedBy: .newlines)
