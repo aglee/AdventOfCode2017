2e3603ea170ae82eebe85706bce33382582be6f5
Andy Lee <aglee@mac.com>
2017-12-25 02:21:48 -0500

    Being fussy. Fixed Day 25 input to match actual input (spaces, not tabs).
    - When I originally pasted the text the spaces got converted to tabs.

--- Day 25 - The Halting Problem/input.swift
+++ Day 25 - The Halting Problem/input.swift
@@ -6,24 +6,24 @@ Begin in state A.
 Perform a diagnostic checksum after 6 steps.
 
 In state A:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state B.
-	If the current value is 1:
-		- Write the value 0.
-		- Move one slot to the left.
-		- Continue with state B.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state B.
 
 In state B:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the left.
-		- Continue with state A.
-	If the current value is 1:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state A.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
 """.components(separatedBy: .newlines)
 
 // Real input.
@@ -32,63 +32,63 @@ Begin in state A.
 Perform a diagnostic checksum after 12173597 steps.
 
 In state A:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state B.
-	If the current value is 1:
-		- Write the value 0.
-		- Move one slot to the left.
-		- Continue with state C.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state C.
 
 In state B:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the left.
-		- Continue with state A.
-	If the current value is 1:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state D.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state D.
 
 In state C:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state A.
-	If the current value is 1:
-		- Write the value 0.
-		- Move one slot to the left.
-		- Continue with state E.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state E.
 
 In state D:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state A.
-	If the current value is 1:
-		- Write the value 0.
-		- Move one slot to the right.
-		- Continue with state B.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the right.
+    - Continue with state B.
 
 In state E:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the left.
-		- Continue with state F.
-	If the current value is 1:
-		- Write the value 1.
-		- Move one slot to the left.
-		- Continue with state C.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state F.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state C.
 
 In state F:
-	If the current value is 0:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state D.
-	If the current value is 1:
-		- Write the value 1.
-		- Move one slot to the right.
-		- Continue with state A.
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state D.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
 """.components(separatedBy: .newlines)
 
